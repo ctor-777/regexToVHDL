48a5f08a1a85c3ac7026d40fa8c23d5423d66677
Hector Rodriguez <ctor777@gmail.com>
2022-02-15 14:32:41 +0100

    Improvements.
    Added gitignore for simulation subproducts.
    Debloated genseqnfa getting rid of states on
    a nfa of only one transition.
    Fixed enable feature, now example also working.

--- genseqnfa.vhdl
+++ genseqnfa.vhdl
@@ -14,27 +14,17 @@ entity genseqnfa is
 end entity;
 
 architecture bahavioral of genseqnfa is
-  type state_type is (s0, s1);
-  signal state: state_type := s0;
 begin
   match:process(clck)
   begin
     if enable='1' and clck'event and clck='1' then
       if reset/= '1' then
-        case state is
-          when s0 =>m <= '0';
-                    if n=req then
-                      state <= s1;
-                    else
-                      state <= s0;
-                    end if;
-          when s1 =>m <= '1';
-                    if n/=req then
-                      state <= s0;
-                    end if;
-        end case;
+        if n=req then
+          m <= '1';
+        else
+          m <= '0';
+        end if;
       else
-        state <= s0;
         m <= '0';
       end if;
     end if;
--- make.sh
+++ make.sh
@@ -2,4 +2,3 @@
 ghdl -a genseqnfa.vhdl seqex.vhdl genseqnfa_tb.vhdl;
 ghdl -e genseqnfa_tb;
 ghdl -r genseqnfa_tb --wave=wave.ghw --stop-time=1us;
-gtkwave wave.ghw
