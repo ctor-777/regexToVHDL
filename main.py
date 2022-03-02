0f4b74f4a4ef52adb99b344bf1d2f95910682840
Hector Rodriguez <ctor777@gmail.com>
2022-03-02 12:29:48 +0100

    Added features.
    Added support for concatenated alternatives.
    Added a function to parse quantifiers(now works with {n} and {min,}).

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def parse_regex(regex):
 
         end = regex.find("]", begin)
 
-        if end == -1:
+        if end == -1 or regex.find("[", begin+1, end) != -1:
             raise ValueError("square braquets opened but never closed")
 
         alternative = "|".join(parse_range(regex[begin+1:end]))
@@ -30,6 +30,8 @@ def parse_regex(regex):
 def parse_range(potential_range):
     parsed_range = ""
     for index, token in enumerate(potential_range):
+        if potential_range[index-1] != "-" and potential_range[index+1] != "-" and token != "-":
+            parsed_range += token
         if token == "-":
             first = potential_range[index-1]
             last = potential_range[index+1]
@@ -37,7 +39,19 @@ def parse_range(potential_range):
             parsed_range += range
     return parsed_range
 
-
-test = "abcd[a-dA-D]fghz[a-e1-3]"
+def parse_iteration(regex):
+    begin = regex.find("{")
+    end = regex.find("}")
+    if begin != -1:
+        if end == -1:
+            raise ValueError("curly braquets opened but never closed")
+        coma_pos = regex.find(",", begin, end)
+        if coma_pos == -1:
+            regex = regex[:begin] + (int(regex[begin+1:end])-1) * regex[begin-1] + parse_iteration(regex[end+1:])
+        else:
+            if regex[coma_pos+1] == "}":
+                regex = regex[:begin] + (int(regex[begin+1:coma_pos])) *regex[begin-1] + "*" + parse_iteration(regex[end+1:])
+    return regex
+test = "abdc{5}xdxd{2,}"
 print(test)
-print(parse_regex(test))
+print(parse_iteration(test))
